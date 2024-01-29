f3104b6745a1f65590ba711c238de32ea2bc09a5
Filip Wojno <filip.wojno03@o2.pl>
2024-01-29 11:25:28 +0100

    auto scroll to the down of the website, then download all images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const https = require("https");
 
-async function run(dataToSearch) {
+const run = async (dataToSearch) => {
   for (let i = 0; i < dataToSearch.length; i++) {
     const browser = await puppeteer.launch({
       headless: false,
@@ -13,7 +13,7 @@ async function run(dataToSearch) {
     const page = await browser.newPage();
     await page.goto(`https://www.pexels.com/pl-pl/szukaj/${dataToSearch[i]}`);
     await page.setViewport({ width: 1920, height: 1080 });
-
+    await scrollDown(page, 1000);
     const images = await page.$$("img");
 
     const srcs = await Promise.all(
@@ -49,7 +49,7 @@ async function run(dataToSearch) {
     await page.close();
     await browser.close();
   }
-}
+};
 
 const getDataFromFile = async () => {
   const dataToSearch = await fs
@@ -57,6 +57,32 @@ const getDataFromFile = async () => {
     .split(",");
 
   run(dataToSearch);
+};
+
+const scrollDown = async (page, maxScrolls) => {
+  await page.evaluate(async (maxScrolls) => {
+    await new Promise((resolve) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var scrolls = 0; // scrolls counter
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        scrolls++; // increment counter
+
+        // stop scrolling if reached the end or the maximum number of scrolls
+        if (
+          totalHeight >= scrollHeight - window.innerHeight ||
+          scrolls >= maxScrolls
+        ) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 50);
+    });
+  }, maxScrolls); // pass maxScrolls to the function
 };
 
 getDataFromFile();
